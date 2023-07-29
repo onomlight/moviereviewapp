f128cce78b1c7d39d3aa336a1e34810dc3c017ad
onomlight <pass041@nate.com>
2023-07-29 23:03:22 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -11,49 +11,120 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text(
-            "Hello Flutter",
-            style: TextStyle(fontSize: 28),
+      home: HomePage(), // 홈페이지 보여주기
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // 영화 제목, 사진 데이터
+    List<Map<String, dynamic>> dataList = [
+      {
+        "category": "엘리멘탈",
+        "imgUrl": "https://i.ibb.co/Msndy5h/image.jpg",
+      },
+      {
+        "category": "미션:임파서블7",
+        "imgUrl": "https://i.ibb.co/frPrKPH/7.jpg",
+      },
+      {
+        "category": "인시디어스: 빨간 문",
+        "imgUrl": "https://i.ibb.co/dJv8PTJ/image.jpg",
+      },
+      {
+        "category": "범죄도시3",
+        "imgUrl": "https://i.ibb.co/7SGnBPw/3.jpg",
+      },
+      {
+        "category": "앤트맨과 와스프: 퀀텀매니아",
+        "imgUrl": "https://i.ibb.co/jRJG2jM/image.jpg",
+      },
+      {
+        "category": "스즈메의 문단속",
+        "imgUrl": "https://i.ibb.co/0F2806Z/image.jpg",
+      },
+    ];
+
+    // 화면에 보이는 영역
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        centerTitle: false,
+        iconTheme: IconThemeData(color: Colors.black),
+        title: Text(
+          "Movie Reviews",
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
           ),
         ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16),
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(32),
-                  child: Image.network(
-                    "https://i.ibb.co/nngK6j3/startup.png",
-                    width: 81,
-                  ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.person_outline),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: "영화 제목을 검색해주세요.",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: '이메일',
-                  ),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {},
                 ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: '비밀번호',
-                  ),
-                ),
-                Container(
-                  width: double.infinity,
-                  margin: EdgeInsets.only(top: 24),
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    child: Text("로그인"),
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
-        ),
+          Divider(height: 1),
+          Expanded(
+            child: ListView.builder(
+              itemCount: dataList.length,
+              itemBuilder: (context, index) {
+                String category = dataList[index]['category'];
+                String imgUrl = dataList[index]['imgUrl'];
+
+                return Card(
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image.network(
+                        imgUrl,
+                        width: double.infinity,
+                        height: 200,
+                        fit: BoxFit.cover,
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 200,
+                        color: Colors.black.withOpacity(0.5),
+                      ),
+                      Text(
+                        category,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 36,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
